d7c06ae573a59c2edea858c98a988b4c09faa048
paul6ix <okporp@gmail.com>
2021-05-14 10:42:26 +0100

    Editing the Django Admin backen

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import ModelProduct, ModelCheckout
 
+
 # Register your models here.
+class OrderDetails(admin.ModelAdmin):
+    list_display = ('name', 'email', 'address', 'city', 'items', 'total')
+    search_fields = ('name',)
+
+
 admin.site.register(ModelProduct)
-admin.site.register(ModelCheckout)
+admin.site.register(ModelCheckout, OrderDetails)
+
+admin.site.site_header = "Shop Manager"
+admin.site.site_title = "Admin Backend"
